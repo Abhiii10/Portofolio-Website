02d5775e03a970c272a702f211432b00ba2c0bda
Abhiii10 <paudelabhi2057@gmail.com>
2023-09-26 22:27:03 +0545

    Nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import react from "react";
+import React from "react";
 import "./index.css";
+import Home from "./routes/Home"
+import About from ".routes/About/"
+import Project from "./routes/Project"
+import Contact from "./routes/Contact"
+
 
 function App() {
   return (
-    <div >
-     
-    </div>
+    <>
+    
+    </>
   );
 }
 
