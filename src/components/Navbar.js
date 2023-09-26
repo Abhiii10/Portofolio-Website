2f96789ac40745a44aca8eb9e9a813848884ed57
Abhiii10 <paudelabhi2057@gmail.com>
2023-09-26 23:55:38 +0545

    Navbar styling

---
+++ src/components/Navbar.js
@@ -0,0 +1,47 @@
+import { Link } from "react-router-dom"
+import "./NavbarStyles.css"
+
+import React, { useState } from 'react'
+
+import { FaBars, FaTimes } from "react-icons/fa";
+const Navbar = () => {
+
+  const [click, setclick]=useState(false)
+  const handleClick = () => setclick(!click);
+
+  return (
+    <div className="header">
+        <Link to="/">
+            <h1>Portfolio</h1>
+        </Link>
+        <ul className="nav-menu">
+            <li>
+                <Link to="/">Home</Link>
+            </li>
+            <li>
+                <Link to="/project">Project</Link>
+            </li>
+            <li>
+                <Link to="/about">About</Link>
+            </li>
+            <li>
+                <Link to="/contact">Contact</Link>
+            </li>
+        </ul>
+        <div className="hamburger" onClick={handleClick}>
+            {click ? ( <FaTimes size={20} style={{color: "#fff"}
+             }/>
+         ) : (
+             <FaBars size={20} style={{color: "#fff"}
+             }/>
+             )} 
+            
+           
+        </div>
+        
+
+    </div>
+  )
+}
+
+export default Navbar
