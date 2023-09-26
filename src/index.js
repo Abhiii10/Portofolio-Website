078fe0ce0699feda08eb01996f76df31e630f956
Abhiii10 <paudelabhi2057@gmail.com>
2023-09-26 22:19:24 +0545

    1

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import { BrowserRouter } from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>,
+ document.getElementById(root)
 );
